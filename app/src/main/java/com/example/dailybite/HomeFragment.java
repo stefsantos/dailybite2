47586d314c491b1ff8b1f92a99a205e205973a4e
alotofnekos <yukinefirablu@gmail.com>
2024-10-19 09:45:27 +0800

    recycler-view meals

--- app/src/main/java/com/example/dailybite/HomeFragment.java
+++ app/src/main/java/com/example/dailybite/HomeFragment.java
@@ -10,10 +10,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Calendar;
 
 public class HomeFragment extends Fragment {
-
+    private RecyclerView recyclerView;
+    private MealAdapter mealAdapter;
+    private List<Meal> mealList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,7 +38,20 @@ public class HomeFragment extends Fragment {
         ImageView plusIconMeal = view.findViewById(R.id.plus_icon_meal);
         plusIconMeal.setOnClickListener(v -> navigateToMealInputWithoutDate());
 
+        //initialize recyclerview-meals
+        recyclerView = view.findViewById(R.id.recyclerView_meals);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mealList = new ArrayList<>();
+        mealList.add(new Meal("Breakfast", "10:45 AM", "531 Cal"));
+        mealList.add(new Meal("Lunch", "03:45 PM", "1024 Cal"));
+
+        mealAdapter = new MealAdapter(mealList);
+        recyclerView.setAdapter(mealAdapter);
+
         return view;
+
+
     }
 
     // Open a calendar to select a date (only opens the calendar, no navigation)
